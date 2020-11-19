dd00efb9c8de7b66abcfeaf3a55c572282360699
Yekaterina Batashov <yekaterina.batashov@live.biu.ac.il>
2020-11-19 20:05:13 +0200

    update user controller

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(cors());
 // app.use(jwt());
 
 // api routes
-app.use('/users', require('./users/users.controller'));
+app.use('/user', require('./user/user.controller'));
 app.use('/calendar', require('./ calendar/calendar.controller'));
 
 
