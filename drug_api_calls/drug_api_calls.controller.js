4da68e86ac11b32fb5beaac5db2e8113e3b1deb0
Yekaterina Batashov <yekaterina.batashov@live.biu.ac.il>
2021-02-22 12:06:05 +0200

    interactions api

--- drug_api_calls/drug_api_calls.controller.js
+++ drug_api_calls/drug_api_calls.controller.js
@@ -3,11 +3,18 @@ const router = express.Router();
 const drugService = require('./drug_api_calls.service');
 
 router.get('/drugByName/:drugName', findDrugByName);
+router.get('/findInteractions/:newRxcui/:drugList', findInteractions);
 
 module.exports = router;
 
 function findDrugByName(req, res, next) {
     drugService.findDrugByName(req.params.drugName)
+        .then(result => res.json(result))
+        .catch(err => next(err));
+}
+
+function findInteractions(req, res, next) {
+    drugService.findInteractions(req.params.newRxcui, req.params.drugList)
         .then(result => res.json(result))
         .catch(err => next(err));
 }
