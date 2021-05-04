6c3d1b1eae97c6e98fb1351d10e150576f2e0b7c
eladg7 <eladgarber@gmail.com>
2021-05-04 22:42:28 +0300

    sending request to get drug by properties to rx

--- _helpers/consts.js
+++ _helpers/consts.js
@@ -1,3 +1,6 @@
 const serverConfig = {'IP': 'localhost', 'PORT': 3000}
+const noRepeatEnd = 0;
+const calendarId = "calendar_id";
+const drugInfoList = "drug_info_list";
 
-module.exports= {serverConfig}
+module.exports = {serverConfig: serverConfig, noRepeatEnd: noRepeatEnd, calendarId: calendarId};
